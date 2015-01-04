60c49f259004f42843e01945f4319d6209bbffb1
hirosuegari <hirosuegari@gmail.com>
2015-01-04 16:20:10 +0900

    固定長分割処理

--- src/main/java/com/github/hirosuegari/conv/Main.java
+++ src/main/java/com/github/hirosuegari/conv/Main.java
@@ -1,10 +1,31 @@
 package com.github.hirosuegari.conv;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+    /**
+     * 半角カナのみ取り扱う前提として、バイト配列は使わない。
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+        // 対象のディレクトリからファイルを読み取る（以下、ファイルがあるだけループ）
 
+        // 対象となるレコードを取得（以下、ファイル内ループ）
+        // 固定長を分割
+
+        // 分割した固定長の中から対象の要素を取得
+
+        // 変換処理
+
+        // 固定長レコード再構築
+
+        // 元のレコード置き換え（ファイル内ループここまで）
+
+        // ファイル保存
     }
 
     private String dateConv(String DateString) {
@@ -12,20 +33,38 @@ public class Main {
     }
 
     private String getDateString() {
-        return null;
+        // TODO
+        return null; // FIXME
     }
 
     private void writeDat() {
-
+        // TODO
     }
 
     private String readDat() {
-        return null;
+        // TODO
+        return null; // FIXME
     }
 
-    private String[] fixedStringParser(String fixedString, int... separateNum) {
+    public List<String> fixedStringParser(String fixedString, List<Integer> separateNum) {
         // 固定長文字列長と分割予定の文字列長と等しいか、大きいことを確認する（大きい場合は残りをそのまま返す）
-        return null;
+        int totalNum = 0;
+        for (Integer integer : separateNum) {
+            totalNum = totalNum + integer;
+        }
+        if (fixedString.length() < 1 || fixedString.length() < totalNum) {
+            return null; // FIXME return exception
+        }
+        // 文字列を分割してリストに詰める
+        List<String> separateStrings = new ArrayList<String>();
+        int next = 0;
+        int end = 0;
+        for (Integer sep : separateNum) {
+            end = next + sep;
+            separateStrings.add(fixedString.substring(next, end));
+            next = end;
+        }
+        return separateStrings;
     }
 
 }
