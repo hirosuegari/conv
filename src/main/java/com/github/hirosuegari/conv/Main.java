6d6e085e9a2007f895794d01b82fc91ee7778536
hirosuegari <hirosuegari@gmail.com>
2015-01-04 12:37:33 +0900

    IFもどき

---
+++ src/main/java/com/github/hirosuegari/conv/Main.java
@@ -0,0 +1,31 @@
+package com.github.hirosuegari.conv;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+    private String dateConv(String DateString) {
+        return null;
+    }
+
+    private String getDateString() {
+        return null;
+    }
+
+    private void writeDat() {
+
+    }
+
+    private String readDat() {
+        return null;
+    }
+
+    private String[] fixedStringParser(String fixedString, int... separateNum) {
+        // 固定長文字列長と分割予定の文字列長と等しいか、大きいことを確認する（大きい場合は残りをそのまま返す）
+        return null;
+    }
+
+}
